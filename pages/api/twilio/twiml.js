6a0cc5df1905bdc3ad28f53c11514d61b9b77ae3
ShopEpstein <contactfire757@gmail.com>
2026-03-08 12:04:34 -0500

    Delete pages/api/twilio/twiml.js

--- pages/api/twilio/twiml.js
+++
@@ -1,7 +0,0 @@
-export default function handler(req, res) {
-  res.setHeader('Content-Type', 'text/xml');
-  res.status(200).send(`<?xml version="1.0" encoding="UTF-8"?>
-<Response>
-  <Pause length="1"/>
-</Response>`);
-}
