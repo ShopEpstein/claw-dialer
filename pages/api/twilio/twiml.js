b7c98f4c547ddb42eb6f7e53fab1353852f3ee8a
ShopEpstein <contactfire757@gmail.com>
2026-03-08 11:31:38 -0500

    Create twiml.js

---
+++ pages/api/twilio/twiml.js
@@ -0,0 +1,7 @@
+export default function handler(req, res) {
+  res.setHeader('Content-Type', 'text/xml');
+  res.status(200).send(`<?xml version="1.0" encoding="UTF-8"?>
+<Response>
+  <Pause length="1"/>
+</Response>`);
+}
