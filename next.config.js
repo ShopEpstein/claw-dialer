521d1c7aaea981a744773f6ed0935245d2bf423e
ShopEpstein <contactfire757@gmail.com>
2026-03-10 02:31:49 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,3 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: false,
-}
-module.exports = nextConfig
+const nextConfig = { reactStrictMode: false };
+module.exports = nextConfig;
